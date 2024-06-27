283b4ebd4ba4bad2b227070eba5dd415c5d91376
ValentinisAlessio <ALESSIO.VALENTINIS@studenti.units.it>
2024-06-27 10:38:51 +0200

    Some on prova

--- prova.py
+++ prova.py
@@ -1,142 +1,109 @@
-from hmmlearn.base import BaseHMM
-
 import numpy as np
-import pandas as pd
 from scipy.stats import gamma
-from sklearn.utils import check_random_state
-
-class BaseGammaHMM(BaseHMM):
-
-    def _get_n_fit_scalars_per_param(self):
-        n_comp = self.n_components
-        n_feat = self.n_features
-        return{
-            's' : n_comp - 1,
-            't' : n_comp * (n_comp-1),
-            'a' : n_comp * n_feat,
-            'b' : n_comp * n_feat
-        }
-    
-    def _compute_log_likelihood(self, X):
-        logprob = np.empty((len(X), self.n_components))
-
-        for c in range(self.n_components):
-            logprob[:, c] = np.sum(gamma.logpdf(X, self.shape_[c], scale=self.scale_[c]), axis=1)
-
-        return logprob
-    
-    def _initialize_sufficient_statistics(self):
-            
-        stats = super()._initialize_sufficient_statistics()
-        stats['post'] = np.zeros(self.n_components)
-        stats['sum_obs'] = np.zeros((self.n_components, self.n_features))
-        stats['prod_obs'] = np.zeros((self.n_components, self.n_features))
-        return stats
-    
-    def _accumulate_sufficient_statistics(self, stats, X, framelogprob,
-                                            posteriors, fwdlattice, bwdlattice):
-            
-            super()._accumulate_sufficient_statistics(
-                stats, X, framelogprob, posteriors, fwdlattice, bwdlattice)
-    
-            print(posteriors)
-            stats['post'] += posteriors.sum(axis=0)
-            stats['sum_obs'] += np.sum(X, axis=0)
-            stats['prod_obs'] = stats['prod_obs'] * np.prod(X, axis=0)
-
-    def _generate_sample_from_state(self, state, random_state):
-        random_state = check_random_state(random_state)
-        return random_state.gamma(self.shape_[state], scale=self.scale_[state])
+from scipy.optimize import minimize
 
 
-class GammaHMM(BaseGammaHMM):
+def _init_params(n_states):
+    # trans_mat = np.random.rand((n_states, n_states))
+    # prob_mat = np.diag(np.zeros(n_states))
+    # initial_prob = np.empty(n_states)
+    # Initialize the transition matrix
+    trans_mat = np.random.rand(n_states, n_states)
+    initial_prob = np.ones(n_states) / n_states
+    gamma_params = np.ones((n_states, 2))
 
-    def __init__(self, n_components=1, startprob_prior=1.0, transmat_prior=1.0, sumobs_prior=1.0,
-                    prodobs_prior=1.0, implementation='log',
-                    algorithm='viterbi', random_state=None, n_iter=10, tol=1e-2,
-                    verbose=False, params='stab', init_params='stab'):
+    return trans_mat, initial_prob, gamma_params
+
+def _construct_trans_mat(params, n_states):
+    trans_mat = np.zeros((n_states, n_states))
+    idx = 0
+    for i in range(n_states):
+        for j in range(n_states -1):
+            trans_mat[i, j] = params[idx]
+            idx += 1
         
-        super().__init__(n_components=n_components, startprob_prior=startprob_prior,
-                            transmat_prior=transmat_prior,
-                            algorithm=algorithm, random_state=random_state, n_iter=n_iter,
-                            tol=tol, verbose=verbose, params=params, init_params=init_params, implementation=implementation)
-        
-        self.sumobs_prior = sumobs_prior
-        self.prodobs_prior = prodobs_prior
+        trans_mat[i, -1] = 1 - trans_mat[i, :-1].sum()
 
-    def _init(self, X, lengths=None):
-        super()._init(X, lengths=lengths)
+    return trans_mat
 
-        self.random_state = check_random_state(self.random_state)
+def _construct_initial_probs(params):
+    initial_probs= np.array(params)
+    initial_probs[-1] = 1 - initial_probs[:-1].sum()
 
-        mean_X=X.mean(axis=0)
-        var_X=X.var(axis=0)
-
-        self.shape_ = self.random_state.gamma(
-            shape=mean_X**2 / var_X, 
-            scale=var_X / mean_X,
-            size=(self.n_components, self.n_features)
-        )
-
-        self.scale_ = self.random_state.gamma(
-            shape=mean_X**2 / var_X,
-            scale=var_X / mean_X,
-            size=(self.n_components, self.n_features)
-        )
-
-    def _check(self):
-        super()._check()
-
-        self.sumobs_prior = np.asarray(self.sumobs_prior, dtype=float)
-        self.prodobs_prior = np.asarray(self.prodobs_prior, dtype=float)
-
-        # if self.sumobs_prior.shape != (self.n_components, self.n_features):
-        #     raise ValueError('sumobs_prior must have shape (n_components, n_features)')
-
-        # if self.prodobs_prior.shape != (self.n_components, self.n_features):
-        #     raise ValueError('prodobs_prior must have shape (n_components, n_features)')
-        
-    def _do_mstep(self, stats):
-        super()._do_mstep(stats)
-
-        sumobs = stats['sum_obs']
-        prodobs = stats['prod_obs']
-        post = stats['post']
+    return initial_probs
 
 
-        self.shape_ = (sumobs + self.sumobs_prior) / (post + 1)
-        self.scale_ = (prodobs + self.prodobs_prior) / (post + 1)
+def _forward(initial_prob, trans_mat, gamma_params, X):
     
+    # trans_mat, initial_prob, gamma_params = _init_params(n_states)
+    # n_states = initial_prob.shape[0]
 
-if __name__=='__main__':
-     
+    n_samples, n_features = X.shape
 
-    something = GammaHMM(n_components=2, random_state=42)
-    # print(something)
+    # print(trans_mat, initial_prob, gamma_params)
 
-    # hulls_df=pd.read_csv("data/hulls_df.csv")
+    _lik = initial_prob * gamma.pdf(X[0], a=gamma_params[:, 0], scale=gamma_params[:, 1])
 
-    # X = hulls_df["HomeHull"].to_numpy().reshape(-1, 1)
-    # Y = hulls_df["AwayHull"].to_numpy().reshape(-1, 1)
+    # print(_lik)
 
-    # X = (X - np.mean(X)) / np.std(X)
-    # Y = (Y - np.mean(Y)) / np.std(Y)
+    for i in range(1, n_samples):
+        _lik = _lik @ trans_mat * gamma.pdf(X[i], a=gamma_params[:, 0], scale=gamma_params[:, 1])
+        # print(_lik)
 
-    # np.random.seed(0)
-    random_state = check_random_state(0)
-    n_samples = 1000
-    n_features = 1
-    n_components = 2
-    X = np.concatenate([
-        gamma.rvs(a = 1, scale=3, size=(n_samples // 2, n_features)),
-        gamma.rvs(a = 3, scale=4, size=(n_samples // 2, n_features))
-    ])
-    # Shuffle X
-    X = X[random_state.permutation(n_samples)]
-    lengths = [n_samples]
+    # print(_lik)
+    # print(_lik.shape)
+    # print(trans_mat.shape)
+    # print(gamma_params.shape)
+    # print(initial_prob.shape)
 
-    X = X.reshape(-1, 1)
+    return _lik.sum()
 
-    _ = something.fit(X)
+def _compute_neg_log_lik(params, X, n_states):
 
-    print('done')
+    shape_params = params[:n_states]
+    scale_params = params[n_states:2*n_states]
+    initial_prob_params = params[2*n_states:3*n_states]
+    trans_mat_params = params[3*n_states:]
+
+    initial_prob = _construct_initial_probs(initial_prob_params)
+    trans_mat = _construct_trans_mat(trans_mat_params, n_states)
+    gamma_params = np.column_stack((shape_params, scale_params))
+
+    lik = _forward(initial_prob, trans_mat, gamma_params, X)
+
+    return -np.log(lik)
+
+if __name__ == "__main__":
+    
+    # generate 10 samples from a gamma distribution
+    X1 = gamma.rvs(a=2, scale=2, size=(100, 1))
+    X2 = gamma.rvs(a=3, scale=1, size=(100, 1))
+    X = np.concatenate((X1, X2), axis=0)
+    
+    random_state = np.random.RandomState(0)
+    X = X[random_state.permutation(200)]
+
+    
+    n_states = 2
+
+    # params = np.array([0.5, 0.5, 1, 1, 0.5, 0.5, 0.7, 0.4])
+    # lik = _compute_neg_log_lik(params, X, n_states)
+    # print(lik)
+
+    # minimize log-likelihood in the parameters
+    initial_shape_params = np.array([2.5, 3.1])
+    initial_scale_params = np.array([2.5, 1.5])
+    initial_initial_prob_params = np.random.rand(n_states )
+    initial_transitions_params = np.random.rand(n_states * (n_states ))
+    initial_params = np.concatenate((initial_shape_params, initial_scale_params, initial_initial_prob_params, initial_transitions_params))
+
+    bounds = [(0, None)] * (2 * n_states) + [(0, 1)] * n_states + [(0, 1)] * (n_states * (n_states))
+    
+    result = minimize(_compute_neg_log_lik, initial_params, args=(X, n_states), bounds=bounds)
+
+    estimated_shape = result.x[:n_states]
+    estimated_scale = result.x[n_states:2*n_states]
+    estimated_initial_prob = _construct_initial_probs(result.x[2*n_states:3*n_states])
+    estimated_trans_mat = _construct_trans_mat(result.x[3*n_states:], n_states)
+
+    print(estimated_shape, estimated_scale, estimated_initial_prob, estimated_trans_mat)
