b64292df484137674d518d9663731ca4c60ec80c
ValentinisAlessio <ALESSIO.VALENTINIS@studenti.units.it>
2024-07-04 15:02:28 +0200

    Moved to class bivariate independent model

---
+++ models/BivariateHMM.py
@@ -0,0 +1,148 @@
+import numpy as np
+import torch
+import pyro
+from pyro import poutine
+import pyro.distributions as dist
+from pyro.infer import SVI, TraceEnum_ELBO
+from pyro.infer.autoguide import AutoDelta
+from pyro.optim import Adam,ClippedAdam
+import pandas as pd
+import tqdm
+
+class BivariateHMM:
+
+    def __init__(self, hidden_states,
+                 probs_initial=None,
+                 probs_x=None,
+                 probs_alpha1=None,
+                 probs_beta1=None,
+                 probs_alpha2=None,
+                 probs_beta2=None):
+        self.hidden_states = hidden_states
+        self.probs_initial = probs_initial
+        self.probs_x = probs_x
+        self.probs_alpha1 = probs_alpha1
+        self.probs_beta1 = probs_beta1
+        self.probs_alpha2 = probs_alpha2
+        self.probs_beta2 = probs_beta2
+        self.posterior = None
+        self.guide = None
+        self.elbo = None
+        self.svi = None
+
+    def pyromodel(self, sequence: torch.tensor, include_prior=True):
+        '''
+        Pyro Model for a Hidden Markov Model with a single univariate observation with Gamma emission distribution.
+        Structure of the model taken from the Pyro documentation:
+        https://pyro.ai/examples/hmm.html
+        
+        INPUTS:
+        - sequence (torch.tensor): A 1-dimensional tensor of observations.
+        - include_prior (bool): If True, include priors for the parameters of the model.
+        '''
+        length = sequence.shape[0]
+        with poutine.mask(mask=include_prior):
+            #---------------------------------------------------------------------
+            # Prior for the initial state probabilities
+            probs_initial = pyro.sample(
+                "probs_initial",
+                dist.Dirichlet(torch.ones(self.hidden_states))
+            )
+            #---------------------------------------------------------------------
+            # Transition probabilities
+            probs_x = pyro.sample(
+                "probs_x",
+                dist.Dirichlet(0.9 * torch.eye(self.hidden_states) + 0.1).to_event(1),
+            )
+            #---------------------------------------------------------------------
+            # Prior for the parameters of emission probabilities 
+            probs_alpha1 = pyro.sample(
+                "probs_alpha1",
+                dist.Gamma(concentration=15.0, rate=0.8).expand([self.hidden_states]).to_event(1)
+            )
+
+            probs_beta1 = pyro.sample(
+                "probs_beta1",
+                dist.Gamma(concentration=1.0, rate=1.0).expand([self.hidden_states]).to_event(1)
+            )
+            probs_alpha2 = pyro.sample(
+                "probs_alpha2",
+                dist.Gamma(concentration=15.0, rate=0.8).expand([self.hidden_states]).to_event(1)
+            )
+
+            probs_beta2 = pyro.sample(
+                "probs_beta2",
+                dist.Gamma(concentration=1.0, rate=1.0).expand([self.hidden_states]).to_event(1)
+            )
+        
+        x = pyro.sample(
+            "x_0",
+            dist.Categorical(probs_initial),
+            infer={"enumerate": "parallel"},
+        )
+        for t in pyro.markov(range(length)):
+            if t > 0:
+                x = pyro.sample(
+                    f"x_{t}",
+                    dist.Categorical(probs_x[x]),
+                    infer={"enumerate": "parallel"},
+                )
+            
+            pyro.sample(
+                f"y1_{t}",
+                dist.Gamma(probs_alpha1[x], probs_beta1[x]),
+                obs=sequence[t, 0]
+            )
+
+            pyro.sample(
+                f"y2_{t}",
+                dist.Gamma(probs_alpha2[x], probs_beta2[x]),
+                obs=sequence[t, 1]
+            )
+
+    def fit(self,sequence,training_steps=500):
+        # clear fit params
+        self.posterior=None
+        self.guide=None
+        self.elbo=None
+        self.svi=None
+        
+        # Set the guide and clear params
+        guide = AutoDelta(poutine.block(self.pyromodel, expose=["probs_initial",
+                                                                     "probs_x",
+                                                                     "probs_alpha1",
+                                                                     "probs_beta1",
+                                                                     "probs_alpha2",
+                                                                     "probs_beta2"]))
+        pyro.clear_param_store()
+
+        # Optimizer
+        optimizer = Adam({"lr": 0.01})
+
+        # Inference algorithm
+        self.elbo = TraceEnum_ELBO(max_plate_nesting=1)
+        self.svi = SVI(self.pyromodel, guide, optimizer, loss=self.elbo)
+
+        # Training
+        tqdm_bar = tqdm.tqdm(range(training_steps))
+        for step in tqdm_bar:
+            loss = self.svi.step(sequence)
+            tqdm_bar.set_postfix({'LOSS': loss})
+            
+        # Update self params
+        self.posterior = guide(sequence)
+        self.probs_initial = self.posterior['probs_initial']
+        self.probs_x = self.posterior['probs_x']
+        self.probs_alpha1 = self.posterior['probs_alpha1']
+        self.probs_beta1 = self.posterior['probs_beta1']
+        self.probs_alpha2 = self.posterior['probs_alpha2']
+        self.probs_beta2 = self.posterior['probs_beta2']
+        
+    def print_estimates(self):
+        for state in range(self.hidden_states):
+            HomeMean= (self.probs_alpha1[state]/self.probs_beta1[state])*100
+            AwayMean= (self.probs_alpha2[state]/self.probs_beta2[state])*100
+            print(f">> Mean of the Convex Hull for home team (STATE {state}): {HomeMean:.2f} m^2")
+            print(f">> Mean of the Convex Hull for away team (STATE {state}): {AwayMean:.2f} m^2")
+            
+            
